3f002639f8b1bb247ea09ea9c22d2ac63bf87249
mhmdhsnjlyly140-eng <mhmdhsnjlyly140@gmail.com>
2026-07-18 11:17:12 +0330

    Update test-db.js

--- api/test-db.js
+++ api/test-db.js
@@ -1,17 +1,7 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
-
-module.exports = async function handler(req, res) {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT NOW()');
-    client.release();
-    res.status(200).json({ success: true, time: result.rows[0] });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+export default function handler(req, res) {
+  res.status(200).json({
+    success: true,
+    message: "API is working!",
+    time: new Date().toISOString()
+  });
+}
