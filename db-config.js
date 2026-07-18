425184b6f14a8fd060640028d6d4947158e3cfe5
mhmdhsnjlyly140-eng <mhmdhsnjlyly140@gmail.com>
2026-07-18 11:47:24 +0330

    Create db-config.js

---
+++ db-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'ep-jolly-lab-azzmo2t3-pooler.c-3.ap-southeast-1.aws.neon.tech',
+  port: 5432,
+  database: 'neondb',
+  user: 'neondb_owner',
+  password: 'npg_ayNhnpBP9Yx5',
+  ssl: { rejectUnauthorized: false }
+};
