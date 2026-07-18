c2d8ccc429146504f824796e648ba9fbb0bb6d4a
mhmdhsnjlyly140-eng <mhmdhsnjlyly140@gmail.com>
2026-07-18 10:55:48 +0330

    Update test-db.js

--- api/test-db.js
+++ api/test-db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'ep-jolly-lab-azzmo2t3-pooler.c-3.ap-southeast-1.aws.neon.tech',
-  port: 5432,
-  database: 'neondb',
-  user: 'neondb_owner',
-  password: 'npg_ayNhnpBP9Yx5',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
